f55ebc2f120facbe475e36228be478ea33167ed1
ulas96 <y.ulas.yildiz@gmail.com>
2023-12-07 18:34:44 +0300

    Home update

---
+++ src/components/Home.jsx
@@ -0,0 +1,23 @@
+import "./style.css";
+
+
+const Home = () => {
+    return (
+        <>
+            <div id="home" className="home">
+                <div className="home-text">
+                    <p>
+                        Welcome, I am Ulaş <br/>
+                        A software developer <br/>
+                        who believes in <br/>
+                        tehcnology shapes the future.
+                    </p>
+
+                </div>
+
+            </div>
+        </>
+    );
+};
+
+export default Home;
