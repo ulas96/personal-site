78ef64f679bf86337ce8526b90b79dbe2b3d2214
ulas96 <y.ulas.yildiz@gmail.com>
2023-12-07 18:34:27 +0300

    Banner update

---
+++ src/components/Banner.jsx
@@ -0,0 +1,15 @@
+import Navbar from "./Navbar.jsx";
+import Home from "./Home.jsx";
+
+const Banner = () => {
+    return (
+        <>
+            <div className="banner">
+                <Navbar></Navbar>
+                <Home></Home>
+            </div>
+        </>
+    );
+};
+
+export default Banner;
