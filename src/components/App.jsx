8ac3be4850b425033aa7c536d70340431d59bbcf
ulas96 <y.ulas.yildiz@gmail.com>
2023-12-11 14:56:45 +0300

    App update

---
+++ src/components/App.jsx
@@ -0,0 +1,24 @@
+// App.js
+
+import { BrowserRouter} from 'react-router-dom';
+import About from "./About.jsx";
+import Projects from "./Projects.jsx";
+import Skills from "./Skills.jsx";
+import Banner from "./Banner.jsx";
+
+
+const App = () => {
+    return (
+        <>
+            <BrowserRouter>
+                <Banner></Banner>
+                <About></About>
+                <Skills></Skills>
+                <Projects></Projects>
+            </BrowserRouter>
+        </>
+
+    );
+};
+
+export default App;
