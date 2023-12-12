6d11d0e43745373344000e27b7dcf0e54bb3e057
ulas96 <y.ulas.yildiz@gmail.com>
2023-12-12 17:24:47 +0300

    constants are in

---
+++ src/constants/index.js
@@ -0,0 +1,4 @@
+export const education = [
+    {institution: 'Ozel Ege Lisesi', department: 'Science/Mathematics', date: '2010-2014'},
+    {institution: 'Middle East Technical University', department: 'Aerospace Engineering', date: '2014-2021'},
+]
