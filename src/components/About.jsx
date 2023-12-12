7ff84e8c82f476658bc5e2a60e18a1654b3f2ab2
ulas96 <y.ulas.yildiz@gmail.com>
2023-12-12 17:25:49 +0300

    About update

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,10 +1,27 @@
 import "./style.css";
+import Timeline from "./Timeline.jsx";
+import {education as events} from "/Users/ulas/Documents/GitHub/personal-site/src/constants/index.js";
 const About = () => {
+
     return (
         <>
             <div className="about" id="about">
                 <div className="about-header">
-                    Who am I?
+
+                </div>
+
+                <div className="about-text">
+                    <div className="education">
+                        <div className="education-header">
+                            Education
+                        </div>
+                        <Timeline events={events}/>
+                    </div>
+
+                    <div className="experience">
+
+                    </div>
+
                 </div>
             </div>
         </>
