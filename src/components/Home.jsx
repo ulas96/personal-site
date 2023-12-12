988af529204c3ed8b3a1afa7537e3af53a83321c
ulas96 <y.ulas.yildiz@gmail.com>
2023-12-12 07:03:41 +0300

    style and image update

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,18 +4,21 @@ import "./style.css";
 const Home = () => {
     return (
         <>
-            <div id="home" className="home">
-                <div className="home-text">
-                    <p>
-                        Welcome, I am Ulaş <br/>
-                        A software developer <br/>
-                        who believes in <br/>
-                        tehcnology shapes the future.
-                    </p>
+            <div id="home">
+                <div className="home">
+                    <div className="home-text">
+                        <p>
+                            Welcome, I am Ulaş <br/>
+                            A software developer <br/>
+                            who believes in <br/>
+                            tehcnology shapes the future.
+                        </p>
+
+                    </div>
 
                 </div>
-
             </div>
+
         </>
     );
 };
