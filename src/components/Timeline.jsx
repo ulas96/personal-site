b549abc6d4f33b7593c8bc9eedcc1c3845d70ea2
ulas96 <y.ulas.yildiz@gmail.com>
2023-12-13 06:57:02 +0300

    Timeline update

--- src/components/Timeline.jsx
+++ src/components/Timeline.jsx
@@ -1,12 +1,17 @@
 
 import {VerticalTimeline, VerticalTimelineElement} from 'react-vertical-timeline-component';
-
+import 'react-vertical-timeline-component/style.min.css';
+import PropTypes from 'prop-types';
 const TimelineEvent = ({ event }) => {
     return (
         <VerticalTimelineElement
         contentStyle={{ background: '#fff', color: '#000' }}
         contentArrowStyle={{ borderRight: '7px solid  #fff' }}
         date={event.date}
+        iconStyle={{ background: '#fff', color: '#fff' }}
+        icon={<div className="timeline-icon-div">
+            <img src={event.icon} alt={event.institution} className="timeline-icon" />
+        </div>}
         >
             <h3 className="vertical-timeline-element-title">{event.institution}</h3>
 
@@ -22,7 +27,7 @@ const Timeline = ({events}) => {
             <div className="timeline">
                 <VerticalTimeline>
                     {events.map((event, index) => (
-                        <TimelineEvent key={`education-${index}`}event={event}/>
+                        <TimelineEvent key={`education-${index}`} event={event}/>
                     ))}
                 </VerticalTimeline>
             </div>
@@ -30,4 +35,23 @@ const Timeline = ({events}) => {
     );
 }
 
+TimelineEvent.propTypes = {
+    event: PropTypes.shape({
+        date: PropTypes.string.isRequired,
+        institution: PropTypes.string.isRequired,
+        department: PropTypes.string.isRequired,
+        icon: PropTypes.string.isRequired,
+    }).isRequired,
+};
+
+Timeline.propTypes = {
+    events: PropTypes.arrayOf(
+        PropTypes.shape({
+            date: PropTypes.string.isRequired,
+            institution: PropTypes.string.isRequired,
+            department: PropTypes.string.isRequired,
+            icon: PropTypes.string.isRequired,
+        })
+    ).isRequired,
+};
 export default Timeline;
