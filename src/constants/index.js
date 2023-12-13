9640efe71137a712b427438a0eb7897b6949966a
ulas96 <y.ulas.yildiz@gmail.com>
2023-12-13 06:56:51 +0300

    assets are added

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,4 +1,11 @@
-export const education = [
-    {institution: 'Ozel Ege Lisesi', department: 'Science/Mathematics', date: '2010-2014'},
-    {institution: 'Middle East Technical University', department: 'Aerospace Engineering', date: '2014-2021'},
-]
+import ensLogo from '../assets/ens-logo.png';
+import ege from '../assets/ege.png';
+import metu from '../assets/metu.png';
+
+
+const education = [
+    {institution: 'Ozel Ege Lisesi', department: 'Science/Mathematics', date: '2010-2014', icon: ege},
+    {institution: 'Middle East Technical University', department: 'Aerospace Engineering', date: '2014-2021', icon: metu},
+];
+
+export {ensLogo, education};
