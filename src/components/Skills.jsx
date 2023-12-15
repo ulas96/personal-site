58b74f0b466f539da7456f1e7b89b078d8d164c5
ulas96 <y.ulas.yildiz@gmail.com>
2023-12-15 15:18:21 +0300

    Skills.jsx update

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,11 +1,27 @@
-import "./style.css"
-
+import {skills} from '../constants/index.js';
 
 const Skills = () => {
     return (
         <>
             <div id="skills">
-                Skills
+                <div className="header" id="skills-header">
+                    Skills
+                </div>
+                <div className="skills-container">
+                    {skills.map((skill, index) => (
+                        <div className="skill" key={`skill-${index}`}>
+                            <div className="skill-div">
+                                <div className="skill-icon">
+                                    <img src={skill.icon} className="skill-icon-img"/>
+                                </div>
+                                <div className="skill-name">
+                                    {skill.name}
+                                </div>
+                            </div>
+
+                        </div>
+                    ))}
+                </div>
             </div>
         </>
     );
