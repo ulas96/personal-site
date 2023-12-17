b1e19c64a50eba167b3a9ab18a2dd6a409f23a51
ulas96 <y.ulas.yildiz@gmail.com>
2023-12-17 20:16:04 +0300

    projects cards are added

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,13 +1,28 @@
-import "./style.css"
+import { projects} from "../constants/index.js";
 
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
 
 const Projects = () => {
     return (
-        <>
-            <div className="projects" id="projects">
-                Projects
+        <div className="projects" id="projects">
+            <div className="header">Projects</div>
+            <div className="projects-container">
+                {projects.map((project, index) => (
+                    <Card key={index} className="project-card" style={{ width: '18rem' }}>
+                        <Card.Img variant="top" src={project.image} />
+                        <Card.Body>
+                            <Card.Title>{project.name}</Card.Title>
+                            <Card.Text>
+                                {project.description}
+                            </Card.Text>
+                            <Button variant="primary" href={project.url}>Go to Project</Button>
+                        </Card.Body>
+                    </Card>
+                ))}
             </div>
-        </>
+
+        </div>
     );
 };
 
